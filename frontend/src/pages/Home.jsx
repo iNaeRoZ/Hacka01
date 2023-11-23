4af1e64edbf5041131356cf90a7f164f8a1ef006
sebastien <romeo.sebastien.dev@gmail.com>
2023-11-23 14:12:47 +0100

    page faite manque navbar et traineau

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="page">
+      <div className="traineau-container">
+        <train>
+          <h2 className="train">traineau</h2>
+        </train>
+        <h1 className="merry">Merry christmas</h1>
+
+        <h3 className="about">About Us</h3>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
