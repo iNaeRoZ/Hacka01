b29b342f0cde96cc7a27bafd47e2ac179884b7b9
iNaeRoZ <naeroz@outlook.fr>
2023-11-23 12:55:04 +0100

    install axios et debut code

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,16 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App";
+import Calendar from "./pages/Calendar";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/Calendar",
+    element: <Calendar />,
   },
 ]);
 
