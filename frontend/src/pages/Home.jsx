e5b99f0ce59325e5e23f3bb48d2e772d57b64978
sebastien <romeo.sebastien.dev@gmail.com>
2023-11-23 16:22:24 +0100

    page about fait manque navbar

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom";
 import React from "react";
 import "./Home.css";
 
 function Home() {
   return (
     <div className="page">
+      <Link className="nav-link" to="/about">
+        <h2>about</h2>
+      </Link>
       <h1 className="merry">Merry christmas</h1>
     </div>
   );
