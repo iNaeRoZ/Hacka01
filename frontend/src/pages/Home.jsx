ee42049073a3048d0ac8e81ab16b4acff42f30b6
sebastien <romeo.sebastien.dev@gmail.com>
2023-11-23 14:45:16 +0100

    nouvelle page accueil

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,14 +4,7 @@ import "./Home.css";
 function Home() {
   return (
     <div className="page">
-      <div className="traineau-container">
-        <train>
-          <h2 className="train">traineau</h2>
-        </train>
-        <h1 className="merry">Merry christmas</h1>
-
-        <h3 className="about">About Us</h3>
-      </div>
+      <h1 className="merry">Merry christmas</h1>
     </div>
   );
 }
